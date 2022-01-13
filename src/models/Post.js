09969072426085423d73f670ac3eb953bc7e3cfa
vietanh1408 <you@example.com>
2022-01-13 23:39:35 +0700

    update create post

--- src/models/Post.js
+++ src/models/Post.js
@@ -1,53 +1,49 @@
 const { Schema, model, Types } = require('mongoose')
 
 const ImageSchema = new Schema({
-  publicId: {
-    type: String,
-    default: '',
-  },
-  url: {
-    type: String,
-    default: '',
-  },
-  _id: false,
+    publicId: {
+        type: String,
+        default: '',
+    },
+    url: {
+        type: String,
+        default: '',
+    },
+    _id: false,
 })
 
-const PostSchema = new Schema(
-  {
+const PostSchema = new Schema({
     user: {
-      type: Types.ObjectId,
-      ref: 'user',
-      required: true,
+        type: Types.ObjectId,
+        ref: 'user',
+        required: true,
     },
     description: {
-      type: String,
-      required: true,
-      max: 1024,
-      default: '',
+        type: String,
+        // required: true,
+        max: 1024,
+        default: '',
     },
     image: {
-      type: ImageSchema,
-      default: {
-        publicId: '',
-        url: '',
-      },
+        type: ImageSchema,
+        default: {
+            publicId: '',
+            url: '',
+        },
     },
     likes: [{ type: Types.ObjectId, ref: 'user' }],
     likeLength: {
-      type: Number,
-      default: 0,
+        type: Number,
+        default: 0,
     },
     commentLength: {
-      type: Number,
-      default: 0,
+        type: Number,
+        default: 0,
     },
-  },
-  {
+}, {
     timestamps: true,
-  },
-  {
+}, {
     versionKey: false,
-  }
-)
+})
 
 module.exports = model('post', PostSchema)
