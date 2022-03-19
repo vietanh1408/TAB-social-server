3f642564f054475e6fe55b2abf84cb63e8715027
vietanhh1408 <14081999lva@gmail.com>
2022-03-19 20:50:27 +0700

    update redis

--- src/jobs/arena.js
+++ src/jobs/arena.js
@@ -2,22 +2,33 @@ require('dotenv').config()
 const Arena = require('bull-arena')
 const Bull = require('bull')
 const { jobType } = require('./jobType')
+const url = require('url')
 
 const queues = [jobType.PROCESS_SEND_VERIFIED_EMAIL]
 
+const getRedisConfig = (redisUrl) => {
+    const redisConfig = url.parse(redisUrl)
+    return {
+        host: redisConfig.hostname || '127.0.0.1',
+        port: Number(redisConfig.port || 6379),
+        db: (redisConfig.pathname || '/0').substr(1) || '0',
+        // password: redisConfig.auth ? redisConfig.auth.split(':')[1] : undefined,
+        maxRetriesPerRequest: null,
+        enableReadyCheck: true,
+        autoResubscribe: true
+    }
+}
+
+const redisConfig = getRedisConfig(
+    process.env.REDIS_TASK_URL || 'redis://localhost:6379/2'
+)
 const arenaQueues = (queues) => {
     if (queues && queues.length > 0) {
         return queues.map((queue) => {
             return {
                 name: queue,
                 hostId: 'Redis',
-                redis: {
-                    port: 6379,
-                    host: 'localhost',
-                    maxRetriesPerRequest: null,
-                    enableReadyCheck: true,
-                    autoResubscribe: true
-                },
+                redis: redisConfig
             }
         })
     }
